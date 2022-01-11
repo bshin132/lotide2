2c3c76e49944c14a4a62f1e380837fb8ee7df8a2
bshin132 <bshin132@gmail.com>
2022-01-11 01:20:53 +0000

    implement head

---
+++ head.js
@@ -0,0 +1,17 @@
+//IMPORTED ASSERTEQUAL FUNCTION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(array) {
+  return array[0];
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), );
+assertEqual(head([5]), 5);
