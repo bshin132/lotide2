02ecf0552da77bda366e61606f72baf316ef4fec
bshin132 <bshin132@gmail.com>
2022-01-17 22:02:53 +0000

    refactor middle

--- middle.js
+++ middle.js
@@ -1,22 +1,5 @@
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
- }
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-}
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
 const middle = function(array) {
   if (array.length < 3) {
@@ -32,5 +15,4 @@ const middle = function(array) {
   }
 }
 
-console.log(middle([1, 2, 4])) // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
+module.exports = middle;
---
+++ test/middleTest.js
@@ -0,0 +1,4 @@
+const middle = require('../middle');
+
+console.log(middle([1, 2, 4])) // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
