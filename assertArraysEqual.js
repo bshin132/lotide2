a0c48adffe8112b42ce5712bfa2549eac92dc94f
bshin132 <bshin132@gmail.com>
2022-01-17 21:59:15 +0000

    refactor assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,4 @@
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
- }
+const eqArrays = require('./eqArrays')
 
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
@@ -18,5 +8,4 @@ const assertArraysEqual = function(arr1, arr2) {
   }
 }
 
-assertArraysEqual(['1', '2', '3'], ['1', '2', '3'])
-assertArraysEqual(['1', '2', 3], ['1', '2', '3'])
+module.exports = assertArraysEqual;
